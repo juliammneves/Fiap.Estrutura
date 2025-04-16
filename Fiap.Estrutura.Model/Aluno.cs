94336e78c55a44f330c2f3b3c8c300dc776467d1
labsfiap <labsfiap@PA105MICRO26>
2025-04-15 22:40:22 -0300

    Add project files.

---
+++ Fiap.Estrutura.Model/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Estrutura.Model
+{
+    public class Aluno : Pessoa
+    {
+        public int rm { get; set; }
+        public decimal vlMensalidade { get; set; }
+    }
+}
---
+++ Fiap.Estrutura.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿using Fiap.Estrutura.Model;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Aluno classeAluno = new Aluno();
+classeAluno.cpf = "1234";
+
+Console.WriteLine("Digite seu usuario: ");
+Console.ReadLine();
+
